d655853676e6c22adc89177072cc4aa7b0d65f78
Indu Prakash <leolite1@gmail.com>
2025-10-08 20:20:44 -0500

    fix: include device name in log when creating

--- custom_components/sensi/coordinator.py
+++ custom_components/sensi/coordinator.py
@@ -666,8 +666,8 @@ class SensiUpdateCoordinator(DataUpdateCoordinator):
                 if icd_id in devices:
                     devices[icd_id].update(device_data)
                 else:
-                    LOGGER.info("Creating device %s", icd_id)
                     devices[icd_id] = SensiDevice(self, device_data)
+                    LOGGER.info("Created device %s (%s)", devices[icd_id].name, icd_id)
 
                 if "capabilities" in device_data:
                     devices[icd_id].update_capabilities(device_data["capabilities"])
